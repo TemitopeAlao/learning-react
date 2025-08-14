a3c5c5b264070e59d0775b44c31a88bf3ae80e7f
TemitopeAlao <alaotemitope36@gmail.com>
2025-08-14 10:29:32 +0100

    add Day 03: Far-away

---
+++ far-away/src/components/App.js
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import Logo from "./Logo";
+import Form from "./Form";
+import PakingList from "./PakingList";
+import Stats from "./Stats";
+function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+  function handleClearList() {
+    const confirmed = window.confirm(
+      "Are you sure you want to delete all items?"
+    );
+    if (confirmed) setItems([]);
+  }
+  return (
+    <div className="app">
+      <Logo />
+      <Form onAddItems={handleAddItems} />
+      <PakingList
+        items={items}
+        onDeleteItem={handleDeleteItem}
+        onToggleItems={handleToggleItem}
+        onClearList={handleClearList}
+      />
+      <Stats items={items} />
+    </div>
+  );
+}
+
+export default App;
